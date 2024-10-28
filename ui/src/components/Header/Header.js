35a27e8351dd395bf79d4bb989253353bd1e37e2
eric-tikhonov <eric.tikhonov@tylertech.com>
2024-10-27 21:51:53 -0500

    add mobile hamburger nav

--- ui/src/components/Header/Header.js
+++ ui/src/components/Header/Header.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../../style.css";
 import "./header.css";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header">
       <nav className="navigation">
         <div className="logo" role="img" aria-label="Home">
           <a href="/"><i className="fas fa-code"></i></a>
         </div>
-        <div className="links">
+        <div className={`menu-toggle ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+          {isOpen ? (
+            <i className="fas fa-times close-icon"></i>
+          ) : (
+            <>
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </>
+          )}
+        </div>
+        <div className={`links ${isOpen ? "active" : ""}`}>
           <ol>
             <li>
               <a href="/#about">About</a>
