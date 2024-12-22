418226c3e8e6f50a5cea9858c8631950e0d552c5
eric-tikhonov <eric.tikhonov@tylertech.com>
2024-12-22 17:17:29 -0600

    update projects section

--- ui/src/components/Projects/Projects.js
+++ ui/src/components/Projects/Projects.js
@@ -1,57 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./projects.css";
+import kitchenAIdeImage from "../../images/KitchenAIde.png";
 
 function Projects() {
-  const [projects, setProjects] = useState([]);
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      const urls = [
-        "https://picsum.photos/200/200?random=1",
-        "https://picsum.photos/200/200?random=2",
-        "https://picsum.photos/200/200?random=3",
-      ];
-
-      const projectData = urls.map((url, index) => ({
-        image: url,
-        title: `Project ${index + 1}`,
-        description: `This is a brief description of Project ${index + 1}.`,
-        technologies: ["React", "Node.js", "CSS"]
-      }));
-
-      setProjects(projectData);
-    };
-
-    fetchProjects();
-  }, []);
-
   return (
     <section id="projects" className="projects-section">
       <h2 className="section-heading">Some Things I've Built</h2>
-      {projects.map((project, index) => (
-        <div key={index} className="project">
+      <div key={1} className="project">
+        <a
+          href="https://kitchenaide.erictikhonov.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
-            src={project.image}
-            alt={project.title}
+            src={kitchenAIdeImage}
+            alt={"KitchenAIde"}
             className="project-image"
           />
-          <div className="project-description">
-            <h3>Featured Project</h3>
-            <h4>{project.title}</h4>
-            <p>{project.description}</p>
-            <div className="project-technologies">
-              {project.technologies.map((tech, index) => (
-                <span key={index} className="tech-pill">{tech}</span>
-              ))}
-            </div>
+        </a>
+        <div className="project-description">
+          <h3>Featured Project</h3>
+          <h4>{"KitchenAIde"}</h4>
+          <p>
+            {
+              "A dynamic, responsive app for managing ingredients and suggesting ingredients and recipes based on input using Claude 2.0 AI with AWS Bedrock, Lambda, DynamoDB, and Amplify."
+            }
+          </p>
+          <div className="project-technologies">
+            <span key={1} className="tech-pill">
+              {"AWS"}
+            </span>
+            <span key={1} className="tech-pill">
+              {"Claude 2.0"}
+            </span>
+            <span key={1} className="tech-pill">
+              {"React"}
+            </span>
           </div>
         </div>
-      ))}
+      </div>
     </section>
   );
 }
 
-//export default Projects;
-export default () => null;
-
-  
+export default Projects;
